6298a45899afab67193a1923364da12e8ec5ad41
Li Manqi <limanqi.lmq@gmail.com>
2020-02-11 16:30:24 +0800

    add jquery (event listener)

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,12 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(function(e) {
+    	//prevent the page from reloading
+    	e.preventDefault();
+    	//In an event handler, $(this) refers 
+    	//to the object that triggers the event
+    	$(this).css("background-color", "#7fff00");
+    });
 }
+
